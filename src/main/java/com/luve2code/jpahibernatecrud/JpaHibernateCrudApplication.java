52c73a86c474d4c688685db9cfe232acbaba7935
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-09-29 12:11:29 +0300

    Save multiObjects With JPA

--- src/main/java/com/luve2code/jpahibernatecrud/JpaHibernateCrudApplication.java
+++ src/main/java/com/luve2code/jpahibernatecrud/JpaHibernateCrudApplication.java
@@ -16,11 +16,14 @@ public class JpaHibernateCrudApplication {
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
         return runner->{
-            creatStudents(studentDAO);
+//            createStudent(studentDAO);
+            creatMultiStudents(studentDAO);
         };
     }
 
-    private void creatStudents(StudentDAO studentDAO) {
+
+
+    private void createStudent(StudentDAO studentDAO) {
         //create student object
         System.out.println("Creating new student object...");
         Student temp = new Student("Haneen","Mohamed","haneen.m.elfeky@gmail.com");
@@ -32,4 +35,19 @@ public class JpaHibernateCrudApplication {
         //display id of the saved student
         System.out.println("Student Saved with id "+temp.getId());
     }
+
+    private void creatMultiStudents(StudentDAO studentDAO) {
+        //create students object
+        System.out.println("Creating new students object...");
+        Student temp = new Student("Haneen","Mohamed","haneen.m.elfeky@gmail.com");
+        Student temp1 = new Student("Mohamed","Ahmed","mohamedelfeky@gmail.com");
+        Student temp2 = new Student("Asmaa","Ali","asmaa.ali@gmail.com");
+
+        //save the student object
+        System.out.println("Save students");
+        studentDAO.save(temp);
+        studentDAO.save(temp1);
+        studentDAO.save(temp2);
+    }
+
 }
